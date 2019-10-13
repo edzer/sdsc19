28259e550891f3e8611eeeb47d97a2068919148b
Edzer Pebesma <edzer.pebesma@uni-muenster.de>
2019-10-13 15:07:25 -0400

    update

--- sdsc.R
+++ sdsc.R
@@ -51,6 +51,22 @@ pts[[1]]
 st_transform(pts[1], "+proj=longlat +datum=WGS84")[[1]]
 
 
+## ----fig.height=3,fig.width=11-------------------------------------------
+par(mfrow = c(1, 3))
+suppressMessages(f0 <- f <- st_filter(nc, nc[10,], .predicate = st_intersects))
+plot(st_geometry(f0), main = "st_intersects")
+plot(st_geometry(f0), col = 'green', add = TRUE)
+plot(st_geometry(nc[10,]), col = NA, border = 'red', add = TRUE, lwd = 3)
+suppressMessages(f <- st_filter(nc, nc[10,], .predicate = st_touches))
+plot(st_geometry(f0), main = "st_touches")
+plot(st_geometry(f), col = 'green', add = TRUE)
+plot(st_geometry(nc[10,]), col = NA, border = 'red', add = TRUE, lwd = 3)
+suppressMessages(sel <- lengths(st_relate(nc, nc[10,], pattern = "F***1****")) > 0)
+plot(st_geometry(f0), main = "st_relate: touch along line")
+plot(st_geometry(nc)[sel], col = 'green', add = TRUE)
+plot(st_geometry(nc[10,]), col = NA, border = 'red', add = TRUE, lwd = 3)
+
+
 ## ----fig.height=3--------------------------------------------------------
 library(rnaturalearth)
 w = ne_countries(returnclass = "sf")
@@ -114,4 +130,52 @@ a1 = st_area(p1) %>% units::set_units(km^2) # segmentized along parallel
 a2 = st_area(p2) %>% units::set_units(km^2) # segmentized along great circle
 d = (a1/a2) %>% units::set_units('%')
 d - units::set_units(100, "%")
+
+
+## ----fig.width=11,fig.height=3.5-----------------------------------------
+par(mfrow=c(1,2))
+par(mar = c(0,0,1,0))
+plot(st_geometry(w[7,1]), col = NA, border = 'orange', main = "equirectangular", asp = 2)
+plot(st_geometry(st_transform(w[7,1], 3031)), col = NA, border = 'orange', main = "polar stereographic")
+
+## ----echo=TRUE-----------------------------------------------------------
+w[7,] %>% st_geometry() %>% st_is_valid()
+w[7,] %>% st_geometry() %>% st_transform(3031) %>% st_is_valid()
+
+
+## ----echo=TRUE-----------------------------------------------------------
+1/3 == (1 - 2/3)
+(x = c(1/3, 1 - 2/3))
+print(x, digits = 20)
+diff(x)
+
+
+## ----echo=TRUE-----------------------------------------------------------
+.Machine$double.eps
+2.2e-16 * units::set_units(110, km) %>% units::set_units(nm)
+
+
+## ----out.width=600-------------------------------------------------------
+knitr::include_graphics('landuse.png')
+
+
+## ----fig.width=10,fig.height=2.4-----------------------------------------
+suppressPackageStartupMessages(library(stars))
+system.file("tif/L7_ETMs.tif", package = "stars") %>% read_stars() %>% `[`(,,,1:4) %>% plot()
+
+
+## ----out.width=1000------------------------------------------------------
+knitr::include_graphics('cube1.png')
+
+
+## ----out.width=1000------------------------------------------------------
+knitr::include_graphics('cube2.png')
+
+
+## ----out.width=800-------------------------------------------------------
+knitr::include_graphics('cube3.png')
+
+
+## ----out.width=800-------------------------------------------------------
+knitr::include_graphics('cube4.png')
 
